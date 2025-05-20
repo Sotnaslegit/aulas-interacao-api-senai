d8efa18251fe8ec1ccc85fd9cff5adbf1b541687
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-05-19 22:20:41 -0300

    feat: formulario-viacep

---
+++ formulario-viacep/js/script.js
@@ -0,0 +1,43 @@
+const cepInput = document.querySelector("#cep")
+
+cepInput.addEventListener("blur", ()=>{
+
+    /* 
+    sobre a regex /\D/g
+    /.../ - Delimita a expressão regular
+    \D - é o inverso de \d, ou seja, "qualquer caractere que não seja dígito" (0-9)
+    g - É a flag global, que faz a substituição ocorrer em todas as ocorrências do texto.
+
+    Exemplo:
+
+    Se o usuário digita 88.000-000
+    A função fará "88.000-000".replace(/\D/g,'') //
+    resultado: '88000000'
+    */
+
+    const cep = cepInput.value.replace(/\D/g,'')
+
+    if(cep.length === 8){
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if(!data.erro){
+                document.querySelector('#logradouro').value = data.logradouro
+
+                document.querySelector('#complemento'). value = data.complemento
+
+                document.querySelector('#bairro').value = data.bairro
+
+                document.querySelector('#cidade').value = data.localidade
+
+                document.querySelector('#estado').value = data.uf
+            } else {
+                console.log("Cep não encontrado")
+            }
+        })
+    } else {
+        alert("CEP inválido")
+    }
+
+})
